a0dd982c54692fbade53aafd66e0ee52544756fc
Lufei1010 <lufeiwu1010@gmail.com>
2025-01-01 22:27:57 +0200

    update homepage navigation UI

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import Container from "../Container";
+import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          {/* children */}
+          <div className="flex flex-row items-cneter justify-between gap-3 md:gap-0">
+            <Logo />
+            <Search />
+            <UserMenu />
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
