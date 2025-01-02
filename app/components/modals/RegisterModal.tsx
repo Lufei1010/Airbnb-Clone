08ab51c342c155179fc1e8436acb44e8b53a8bba
Lufei1010 <lufeiwu1010@gmail.com>
2025-01-02 22:38:58 +0200

    Complete register modal UI

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,16 +1,16 @@
-'use client'
+"use client";
 
-import axios from 'axios';
-import { AiFillGithub } from 'react-icons/ai';
-import { FcGoogle } from 'react-icons/fc';
-import { useCallback, useState } from 'react';
-import {
-  FieldValues,
-  SubmitHandler,
-  useForm
-} from 'react-hook-form';
-import useRegisterModal from '@/app/hooks/useRegisterModal';
-import Modal from './Modal';
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import Modal from "./Modal";
+import Heading from "../Heading";
+import Input from "../input/Input";
+import toast from "react-hot-toast";
+import Button from "../Button";
 const RegisterModal = () => {
   const registerModal = useRegisterModal(); //Accessing the modal state and actions from the store.
   const [isLoading, setIsLoading] = useState(false);
@@ -35,12 +35,69 @@ const RegisterModal = () => {
         registerModal.onClose(); // Close the modal after successful registration
       })
       .catch((error) => {
-        console.log(error);
+        toast.error("Something went wrong.");
       })
       .finally(() => {
         setIsLoading(false);
       });
   };
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome to Airbnb" subtitle="Create an account" />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        type="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Continue with GitHub"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div className="justify-center text-neutral-500 text-center mt-4 font-light">
+        <div className="flex flex-row items-center gap-2">
+          <div>Already have an account?</div>
+          <div onClick={registerModal.onClose} className="text-neutral-800 cursor-pointer hover:underline">
+            Log in
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 
   return (
     <Modal
@@ -50,9 +107,11 @@ const RegisterModal = () => {
       actionLabel="Continue"
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
     />
   );
-}
+};
 
 export default RegisterModal;
 
